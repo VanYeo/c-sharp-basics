270a34867937bfee64494a050f9a71156b633e46
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-20 17:09:12 +0700

    learning concat and verbatim string

--- Program.cs
+++ Program.cs
@@ -17,6 +17,25 @@ namespace practice_dotnet
             Console.WriteLine(result);
             // var numbers = new int[3];
             // numbers[0] = 1;
+          
+
+            var firstName = "Mosh";
+            String lastName = "Joe";
+            //method 1
+            var fullName = firstName + "" + lastName;
+
+            //method 2
+            var myFullName = string.Format("My fullname is  {0} {1}", firstName, lastName);
+            var names = new string[3] { "J", "K", "Q" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            // verbatim string to avoid formatting errors
+            var text = @"Hi John
+            Look into the following paths
+            c:\folder1\folder2
+            c:\folder3\folder4";
+            Console.WriteLine(text);
 
         }
     }
