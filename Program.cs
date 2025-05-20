8f9963aae9e110309d5296158a1f4e29f2613938
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-20 18:25:19 +0700

    learn enum

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Configuration.Assemblies;
 using practice_dotnet.Math;
 
 namespace practice_dotnet
 {
+        public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -17,7 +25,7 @@ namespace practice_dotnet
             Console.WriteLine(result);
             // var numbers = new int[3];
             // numbers[0] = 1;
-          
+
 
             var firstName = "Mosh";
             String lastName = "Joe";
@@ -37,6 +45,33 @@ namespace practice_dotnet
             c:\folder3\folder4";
             Console.WriteLine(text);
 
+        }
+
+        static void Shipping(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            // convvert int to string
+            Console.WriteLine(method.ToString());
+
+            // converts string into express into common value
+            var methodName = "Express";
+
+            // cast from object to actual enum type shipping method 
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+
+        static void Increments(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}. b: {1}", a, b));
         }
     }
 }
