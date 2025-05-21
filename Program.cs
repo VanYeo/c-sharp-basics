97c8801b1ca6a4bc77a3cbfb975c673dbf49bef5
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-05-21 14:11:44 +0700

    practice questions for if else statements

--- Program.cs
+++ Program.cs
@@ -1,182 +1,107 @@
 ﻿using System;
 using System.Configuration.Assemblies;
+using System.Globalization;
 using practice_dotnet.Math;
 
 
 namespace Conditionals
 {
-    public enum Season
-    {
-        Spring,
-        Summer,
-        Autumn,
-        Winter
-    }
     class Program
     {
-        static void Seasons(string[] args)
+        // 1- Write a program and ask the user to enter a number. 
+        // The number should be between 1 to 10. 
+        // If the user enters a valid number, display "Valid" on the console. 
+        // Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+        static void ValidateNumber(string[] args)
         {
-            var season = Season.Autumn;
-            switch (season)
+            Console.Write("Enter a number: ");
+            string? input = Console.ReadLine();
+            int number = int.Parse(input);
+            if (number >= 1 && number <= 10)
             {
-                // diff cases
-                case Season.Autumn:
-                    Console.WriteLine("It's autumn and a beautiful season.");
-                    break;
-                case Season.Summer:
-                    Console.WriteLine("It's beach weather!");
-                    break;
-
-                // when season is none of the values
-                default:
-                    Console.WriteLine("I don't understand that season!");
-                    break;
+                Console.WriteLine("Valid");
             }
-            //returns Autumn
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
-        static void Main(string[] args)
+
+        //2- Write a program which takes two numbers from the console and displays the maximum of the two.
+        static void DisplayMax(string[] args)
         {
-            int hour = 10;
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning.");
-            }
+            Console.Write("Enter number 1");
+            string? input1 = Console.ReadLine();
 
-            else if (hour >= 12 && hour < 18)
+            Console.Write("Enter number 2");
+            string? input2 = Console.ReadLine();
+
+            int num1 = int.Parse(input1);
+            int num2 = int.Parse(input2);
+
+            int max = Math.Max(num1, num2);
+            Console.WriteLine(max);
+        }
+
+        //3- Write a program and ask the user to enter the width and height of an image. 
+        // Then tell if the image is landscape or portrait.
+        static void ImageOrientation(string[] args)
+        {
+            Console.Write("Enter width");
+            string? input1 = Console.ReadLine();
+            int width = int.Parse(input1);
+
+            Console.Write("Enter height");
+            string? input2 = Console.ReadLine();
+            int height = int.Parse(input2);
+
+            if (height > width)
             {
-                Console.WriteLine("It's afternoon.");
+                Console.WriteLine("It is portrait");
             }
             else
             {
-                Console.WriteLine("It's evening.");
+                Console.WriteLine("It is landscape");
             }
+        }
+        /* 4- Your job is to write a program for a speed camera. 
+        For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. 
+        Write a program that asks the user to enter the speed limit. 
+        Once set, the program asks for the speed of a car. 
+        If the user enters a value less than the speed limit, 
+        program should display Ok on the console. If the value is above the speed limit,
+        the program should calculate the number of demerit points. 
+        For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console.
+        If the number of demerit points is above 12, the program should display License Suspended. */
 
-            bool isGoldCustomer = true;
+        static void SpeedLimit(string[] args)
+        {
+            Console.Write("Enter speed limit: ");
+            string? input1 = Console.ReadLine();
+            var speedLimit = int.Parse(input1);
 
-            //method 1: if else statements
-            float price;
-            if (isGoldCustomer)
+            Console.Write("Enter speed of car: ");
+            string? input2 = Console.ReadLine();
+            var carSpeed = int.Parse(input2);
+
+            if (carSpeed < speedLimit)
             {
-                price = 19.95f;
+                Console.WriteLine("Ok");
             }
             else
             {
-                price = 29.95f;
+                var demeritPoints = Decimal.Round((carSpeed - speedLimit) / 5);
+                if (demeritPoints > 12)
+                {
+                    Console.WriteLine("License Suspended");
+                }
+                else
+                {
+                    Console.WriteLine(demeritPoints + " incurred.");
+                }
             }
 
-            //method 2: single line if else statement
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
         }
     }
 }
-// namespace practice_dotnet
-// {
-//         public enum ShippingMethod
-//     {
-//         RegularAirMail = 1,
-//         RegisteredAirMail = 2,
-//         Express = 3
-//     }
-
-//     public class Person
-//     {
-//         public int Age;
-//     }
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             var john = new Person();
-//             john.FirstName = "John";
-//             john.LastName = "Smith";
-//             john.Introduce();
-
-//             Calculator calculator = new Calculator();
-//             var result = calculator.Add(1, 2);
-//             Console.WriteLine(result);
-//             // var numbers = new int[3];
-//             // numbers[0] = 1;
-
-
-//             var firstName = "Mosh";
-//             String lastName = "Joe";
-//             //method 1
-//             var fullName = firstName + "" + lastName;
-
-//             //method 2
-//             var myFullName = string.Format("My fullname is  {0} {1}", firstName, lastName);
-//             var names = new string[3] { "J", "K", "Q" };
-//             var formattedNames = string.Join(",", names);
-//             Console.WriteLine(formattedNames);
-
-//             // verbatim string to avoid formatting errors
-//             var text = @"Hi John
-//             Look into the following paths
-//             c:\folder1\folder2
-//             c:\folder3\folder4";
-//             Console.WriteLine(text);
-
-//         }
-
-//         static void Shipping(string[] args)
-//         {
-//             var method = ShippingMethod.Express;
-//             Console.WriteLine((int)method);
-
-//             var methodId = 3;
-//             Console.WriteLine((ShippingMethod)methodId);
-
-//             // convert int to string
-//             Console.WriteLine(method.ToString());
-
-//             // converts string into express into common value
-//             var methodName = "Express";
-
-//             // cast from object to actual enum type shipping method 
-//             var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-//             (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-//         }
-
-//         static void refenceType(string[] args)
-//         {
-//             //int: b is a separate copy of a and does not get referenced
-//             var a = 10;
-//             var b = a;
-//             b++;
-//             Console.WriteLine(string.Format("a: {0}. b: {1}", a, b));
-//             //a: 10, b: 11
-
-//             //arrarys: both arrays point to the same copy of array in heap
-//             var array1 = new int[3] { 1, 2, 3 };
-//             var array2 = array1;
-//             array2[0] = 0;
-//             Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
-//             // gives both 0
-//         }
-
-//         static void Main(string[] args)
-//         {
-//             var number = 1;
-//             Increment(number);
-//             Console.WriteLine(number);
-//             // gives number = 1 as separate copy is made in Increment method
-
-//             var person = new Person() { Age = 20 };
-//             MakeOld(person);
-//             Console.WriteLine(person.Age);
-//             // gives 30 as reference same obj on heap
-
-//         }
-
-//         public static void Increment(int number)
-//         {
-//             number += 10;
-//         }
-
-//         public static void MakeOld(Person person)
-//         {
-//             person.Age += 10;
-//         }
-//     }
-// }
-
